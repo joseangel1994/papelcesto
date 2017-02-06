7f9fdd3571fca08616e4cdcfc07c30028dc855c5
TS-EU\jexposi1 <JOSE-ANGEL.Exposito@t-systems.com>
2017-02-06 17:06:39 +0100

    feat: creado dto Player, con create y update, y mapper de Player

---
+++ src/main/java/com/tommyrot/papelcesto/dto/create/CreatePlayer.java
@@ -0,0 +1,10 @@
+package com.tommyrot.papelcesto.dto.create;
+
+import lombok.Data;
+
+@Data
+public class CreatePlayer {
+
+  private String name;
+  private String performanceId;
+}
---
+++ src/main/java/com/tommyrot/papelcesto/mappers/PlayerMapper.java
@@ -0,0 +1,34 @@
+package com.tommyrot.papelcesto.mappers;
+
+import org.springframework.stereotype.Component;
+
+import com.tommyrot.papelcesto.domain.PlayerEntity;
+import com.tommyrot.papelcesto.dto.Player;
+import com.tommyrot.papelcesto.dto.create.CreatePlayer;
+import com.tommyrot.papelcesto.dto.update.UpdatePlayer;
+
+@Component
+public class PlayerMapper {
+
+  public PlayerEntity toRunnerEntity(CreatePlayer createPlayer) {
+    PlayerEntity playerToCreate = new PlayerEntity();
+    playerToCreate.setName(createPlayer.getName());
+    playerToCreate.setPerformanceId(createPlayer.getPerformanceId());
+    return playerToCreate;
+  }
+
+  public PlayerEntity updateRunnerEntity(UpdatePlayer updatePlayer, PlayerEntity playerToCreate) {
+    playerToCreate.setPlayerId(updatePlayer.getPlayerId());
+    playerToCreate.setName(updatePlayer.getName());
+    playerToCreate.setPerformanceId(updatePlayer.getPerformanceId());
+    return playerToCreate;
+  }
+
+  public Player toPlayer(PlayerEntity playerToReturn) {
+    Player target = new Player();
+    target.setPlayerId(playerToReturn.getPlayerId());
+    target.setName(playerToReturn.getName());
+    target.setPerformanceId(playerToReturn.getPerformanceId());
+    return target;
+  }
+}
