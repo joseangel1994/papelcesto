a65618ebd98c2be11c1a2edf315de7edf720e922
Miguel Rodríguez Ortega <maysunderground@gmail.com>
2017-02-06 15:59:41 +0100

    Inital commit.

---
+++ src/main/java/com/tommyrot/papelcesto/PapelcestoApplication.java
@@ -0,0 +1,12 @@
+package com.tommyrot.papelcesto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapelcestoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapelcestoApplication.class, args);
+	}
+}
