4dc01fba2721028a4759d40eafa2444a029a6167
TS-EU\jexposi1 <JOSE-ANGEL.Exposito@t-systems.com>
2017-02-07 08:58:50 +0100

    feat: completado crud y repository de Player

--- src/main/java/com/tommyrot/papelcesto/repositories/PlayerRepository.java
+++ src/main/java/com/tommyrot/papelcesto/repositories/PlayerRepository.java
@@ -1,5 +1,7 @@
 package com.tommyrot.papelcesto.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tommyrot.papelcesto.domain.PlayerEntity;
@@ -7,5 +9,7 @@ import com.tommyrot.papelcesto.domain.PlayerEntity;
 public interface PlayerRepository extends JpaRepository<PlayerEntity, Long> {
 
   PlayerEntity findPlayerById(long playerId);
+
+  List<PlayerEntity> findByNameContains(String filter);
 
 }
