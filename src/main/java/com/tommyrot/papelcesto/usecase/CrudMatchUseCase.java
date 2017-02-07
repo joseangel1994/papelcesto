638674a9f91a22eb660d6411c0af3f6c7caa7f0c
Miguel Rodríguez Ortega <maysunderground@gmail.com>
2017-02-07 09:30:05 +0100

    feat: finalizacion crud match

--- src/main/java/com/tommyrot/papelcesto/usecase/CrudMatchUseCase.java
+++ src/main/java/com/tommyrot/papelcesto/usecase/CrudMatchUseCase.java
@@ -1,9 +1,15 @@
 package com.tommyrot.papelcesto.usecase;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tommyrot.papelcesto.domain.MatchEntity;
 import com.tommyrot.papelcesto.dto.Match;
+import com.tommyrot.papelcesto.dto.create.CreateMatch;
+import com.tommyrot.papelcesto.dto.update.UpdateMatch;
 import com.tommyrot.papelcesto.mappers.MatchMapper;
 import com.tommyrot.papelcesto.repositories.MatchRepository;
 
@@ -22,9 +28,42 @@ public class CrudMatchUseCase {
 
   public Match getMatchById(Integer id) {
 
-    Match target = mapper.toMatchEntity(id);
+    MatchEntity matchToReturn = matchRepository.findOne(id);
+    Match target = mapper.toMatch(matchToReturn);
 
     return target;
+  }
+
+  public List<Match> getAllMatches() {
+
+    List<MatchEntity> matchList = matchRepository.findAll();
+    List<Match> listToReturn = new LinkedList<Match>();
+
+    for (MatchEntity match : matchList) {
+      listToReturn.add(mapper.toMatch(match));
+    }
+
+    return listToReturn;
+  }
+
+  public Match createNewMatch(CreateMatch createMatch) {
+
+    MatchEntity matchToCreate = mapper.toMatchEntity(createMatch);
+    matchRepository.save(matchToCreate);
+
+    return mapper.toMatch(matchToCreate);
+  }
+
+  public Match updateMatch(Integer id, UpdateMatch updateMatch) {
+
+    MatchEntity matchToUpdate = matchRepository.findOne(id);
+    mapper.updateMatchEntity(matchToUpdate, updateMatch);
+
+    return mapper.toMatch(matchToUpdate);
+  }
+
+  public void deleteMatch(Integer id) {
+    matchRepository.delete(id);
   }
 
 
