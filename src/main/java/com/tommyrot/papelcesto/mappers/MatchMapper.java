3c3ddd3328ceab3dfc1df5e432d1f270fbe7f650
Miguel Rodríguez Ortega <maysunderground@gmail.com>
2017-02-07 09:29:48 +0100

    feat: mapper para match

--- src/main/java/com/tommyrot/papelcesto/mappers/MatchMapper.java
+++ src/main/java/com/tommyrot/papelcesto/mappers/MatchMapper.java
@@ -1,18 +1,41 @@
 package com.tommyrot.papelcesto.mappers;
 
+import org.springframework.stereotype.Component;
+
 import com.tommyrot.papelcesto.domain.MatchEntity;
 import com.tommyrot.papelcesto.dto.Match;
+import com.tommyrot.papelcesto.dto.create.CreateMatch;
+import com.tommyrot.papelcesto.dto.update.UpdateMatch;
 
+@Component
 public class MatchMapper {
 
-  public Match toMatchEntity(Integer id) {
-    MatchEntity source = new MatchEntity();
+  public Match toMatch(MatchEntity source) {
+
     Match target = new Match();
 
-    target.setId(id);
+    target.setId(source.getId());
     target.setDate(source.getDate());
 
     return target;
   }
+
+  public MatchEntity toMatchEntity(CreateMatch source) {
+
+    MatchEntity matchToCreate = new MatchEntity();
+
+    matchToCreate.setDate(source.getDate());
+
+    return matchToCreate;
+  }
+
+  public MatchEntity updateMatchEntity(MatchEntity matchToUpdate, UpdateMatch updateMatch) {
+
+    matchToUpdate.setDate(updateMatch.getDate());
+
+    return matchToUpdate;
+  }
+
+
 
 }
