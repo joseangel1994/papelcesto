2c88958bcecc57f741ba62194d76cfc93d8edad0
Francisco Rodrigo <franjrg94@gmail.com>
2017-02-07 09:25:16 +0100

    feat: Controlar salida del controlador Performance con ResponseEntity.

--- src/main/java/com/tommyrot/papelcesto/controllers/PerformanceController.java
+++ src/main/java/com/tommyrot/papelcesto/controllers/PerformanceController.java
@@ -3,6 +3,8 @@ package com.tommyrot.papelcesto.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,7 +16,7 @@ import com.tommyrot.papelcesto.usecase.CrudPerformance;
 @RestController
 @RequestMapping(value = "/player/{id}/performances")
 public class PerformanceController {
-	
+
 	private final CrudPerformance crudPerformance;
 
 	@Autowired
@@ -22,22 +24,30 @@ public class PerformanceController {
 		super();
 		this.crudPerformance = crudPerformance;
 	}
-	
+
 	@RequestMapping(method = RequestMethod.GET)
-	public List<Performance> getPerformancesByPlayerId(@PathVariable("playerId") Integer playerId){
-		return crudPerformance.getPerformancesByPlayer(playerId);
+	public ResponseEntity<List<Performance>> getPerformancesByPlayerId(@PathVariable("playerId") Integer playerId) {
+		List<Performance> performances = crudPerformance.getPerformancesByPlayer(playerId);
+
+		if (performances.isEmpty()) {
+			return new ResponseEntity<List<Performance>>(performances, HttpStatus.CONFLICT);
+		} else {
+			return new ResponseEntity<List<Performance>>(performances, HttpStatus.OK);
+		}
 	}
-	
-//	public Performance createPerformance(CreatePerformance performanceToCreate){
-//		return crudPerformance.createPerformance(performanceToCreate);
-//	}
-//	
-//	public Performance updatePerformance(Integer id, Performance performanceToUpdate){
-//		return crudPerformance.updatePerformance(id, performanceToUpdate);
-//	}
-//
-//	public void deletePerformance(Integer id){
-//		crudPerformance.deletePerformance(id);
-//	}
-	
+
+	// public Performance createPerformance(CreatePerformance
+	// performanceToCreate){
+	// return crudPerformance.createPerformance(performanceToCreate);
+	// }
+	//
+	// public Performance updatePerformance(Integer id, Performance
+	// performanceToUpdate){
+	// return crudPerformance.updatePerformance(id, performanceToUpdate);
+	// }
+	//
+	// public void deletePerformance(Integer id){
+	// crudPerformance.deletePerformance(id);
+	// }
+
 }
